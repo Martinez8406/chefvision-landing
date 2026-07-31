fda8486e732c45bb8501516777faf3ea46abe4de
Marcin Koniuszko <koniuszkomarcin@gmail.com>
2026-07-31 10:22:20 +0200

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Inter, Montserrat, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import { LanguageProvider } from '@/lib/language-context'
 import './globals.css'
+
+const FB_PIXEL_ID = '1950565928953303'
 
 const _inter = Inter({ subsets: ["latin"] });
 const _montserrat = Montserrat({ subsets: ["latin"] });
@@ -31,6 +34,29 @@ export default function RootLayout({
   return (
     <html lang="pl" className="scroll-smooth" data-scroll-behavior="smooth">
       <body className="font-sans antialiased">
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         <LanguageProvider>
           {children}
         </LanguageProvider>
